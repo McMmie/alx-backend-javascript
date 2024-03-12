45861ebed52d11439c38e1826a2b8f294e3ee1ed
McMmie <tharausamuel01@gmail.com>
2024-03-12 09:47:54 +0000

    function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+	const list = [];
+	list.push({'id': 1, 'firstName': 'Guillaume', 'location': 'San Fransisco'});
+	list.push({'id': 2, 'firstName': 'James', 'location': 'Colombia'});
+	list.push({'id': 5, 'firstName': 'Serena', 'location': 'San Fransisco'});
+	return list;
+}
