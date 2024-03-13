2a7ce1528a9e00e46f16be3f0f14224bb3514601
McMmie <tharausamuel01@gmail.com>
2024-03-13 01:57:34 +0000

    advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,18 +1,17 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-	const { protocol, name } = endpoint;
-	const key = {protocol, name};
+  const { protocol, name } = endpoint;
+  const key = { protocol, name };
 
-	if (!weakMap.has(key)) {
-		weakMap.set(key, 1);
-	} else {
-		const count = weakMap.get(key);
-		weakMap.set(key, count + 1);
-		// Check if the number of queries is >= 5
-		if (count + 1 >= 5) {
-			throw new Error(`Endpoint load is high`);
-		}
-	}
+  if (!weakMap.has(key)) {
+    weakMap.set(key, 1);
+  } else {
+    const count = weakMap.get(key);
+    weakMap.set(key, count + 1);
+    // Check if the number of queries is >= 5
+    if (count + 1 >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
 }
-
