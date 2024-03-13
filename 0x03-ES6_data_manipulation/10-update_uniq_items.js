633f08ebf7c21c4e8858537459aef2d829697fa1
McMmie <tharausamuel01@gmail.com>
2024-03-13 01:04:39 +0000

    more map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+	try {
+		map.forEach((value, key) => {
+			if (value === 1) {
+				map.set(key, 100);
+			}
+		});
+		return map;
+	} catch (e) {
+		throw new Error("Cannot process");
+	}
+}
+
